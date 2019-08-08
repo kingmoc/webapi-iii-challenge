7a4e9108471dea17d800321e670313db51971105
kingmoc <joe.c.a.garcia@gmail.com>
2019-08-08 03:16:48 -0500

    Updated MVP! - Fixed GET /api/users/123

--- users/userRouter.js
+++ users/userRouter.js
@@ -17,36 +17,19 @@ router.get('/', (req, res) => {
 
 router.get('/:id', validateUserId, (req, res) => {
     
-    // console.log(req.test)
     res.status(200).json(req.user)
-
-    // req.user.id = req.params.id
-    // const user = req.user
-
-    // console.log(user)
-    // res.status(200).json(req.user)
-
-
-
-    // Users.getById(userId)
-    //     .then(user => {
-    //         // console.log("FindbyId:", user)
-    //         if(!user) {
-    //             res.status(404).json({ message: "The user with the specified ID does not exist." }) 
-    //         } else {
-    //             res.status(200).json(user)
-    //         }
-    //     })
-    //     .catch(err => {
-    //         res.status(500).json({error: "The user information could not be retrieved."})
-    //     })
+    
 });
 
 router.get('/:id/posts', validateUserId, (req, res) => {
 
     Users.getUserPosts(req.params.id)
         .then(post => {
+            if(!post.id) {
+                res.status(200).json({ message: "User Doesn't Have any Post :( "})
+            } else {
             res.status(200).send(post)
+            }
         })
         .catch(err => {
             res.status(500).json( { error: "The post information could not be retrieved." })
