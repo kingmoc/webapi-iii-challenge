a1c07f424cc1b4c8329d15087bbfb7e829812015
kingmoc <joe.c.a.garcia@gmail.com>
2019-08-07 19:23:05 -0500

    Logger Middleware Implemented Succesfully

--- server.js
+++ server.js
@@ -2,15 +2,26 @@ const express = require('express')
 
 const server = express();
 
+const logger = (req, res, next) => {
+  const { method, url, timestamp } = req
+
+  console.table({
+    "HTTP Request Type": method,
+    "Endpoint URL": url,
+    "Request Timestamp": timestamp
+  })
+  next()
+}
+
+
+server.use(logger)
+
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`)
 });
 
 //custom middleware
 
-function logger(req, res, next) {
-
-};
 
 module.exports = server;
 
