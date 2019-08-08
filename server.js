f2ea91cff72cb3cb91f0ea353c6612a354dffe6b
kingmoc <joe.c.a.garcia@gmail.com>
2019-08-08 02:55:54 -0500

    MVP

--- server.js
+++ server.js
@@ -21,12 +21,6 @@ const logger = (req, res, next) => {
 server.use(logger)
 server.use('/api/users', userRouter)
 
-// server.get('/', (req, res) => {
-//   res.send(`<h2>Let's write some middleware!</h2>`)
-// });
-
-//custom middleware
-
 
 module.exports = server;
 
